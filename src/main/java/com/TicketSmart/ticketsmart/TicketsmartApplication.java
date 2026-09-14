24710252f155c0cdd6d536f9c680cb5ac8116fdd
Archontia Samara <samararxontia@gmail.com>
2026-09-14 18:00:06 +0300

    Initial commit -Smart Ticketing Project

---
+++ src/main/java/com/TicketSmart/ticketsmart/TicketsmartApplication.java
@@ -0,0 +1,13 @@
+package com.TicketSmart.ticketsmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TicketSmart/ticketsmart/TicketsmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TicketSmart.ticketsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
